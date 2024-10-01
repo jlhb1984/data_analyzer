dae4e1e24e03a47a2fbea6e25a29c7caa372dd74
jlhb1984 <jlhb1984@gmail.com>
2024-10-01 15:49:49 -0500

    Prueba exitosa 2

--- data_analyzer02.py
+++ data_analyzer02.py
@@ -1,21 +1,23 @@
 import pandas as pd
 import numpy as np
 
-#Carga de la tabla 1.
+#Carga de la tabla 1. Landmarks InOut.csv
 table01=input("Digita el nombre de la tabla 1: ")
 df_book_landmarks=pd.read_csv(table01)
 df_book_landmarks.info()
 
-#Carga de la tabla 2.
+#Carga de la tabla 2. Geofences InOut Report.csv
 table02=input("Digita el nombre de la tabla 2: ")
-df_book_geofences=pd.read_csv('Geofences InOut Report.csv')
+df_book_geofences=pd.read_csv(table02)
 df_book_geofences.info()
 
+#Carga de la columna de la tabla 1. ContactName
 table01_column=input("Digita el nombre de la columna de la tabla 1: ")
 landmarks=df_book_landmarks.loc[:,table01_column]
 landmarks.info()
 print(landmarks)
 
+#Carga de la columna de la tabla 1. GeofenceName
 table02_column=input("Digita el nombre de la columna de la tabla 2: ")
 geofences=df_book_geofences.loc[:,table02_column]
 geofences.info()
