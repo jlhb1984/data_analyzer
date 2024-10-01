10d909637bd0429292414637b77e138325c1300a
jlhb1984 <jlhb1984@gmail.com>
2024-10-01 15:00:28 -0500

    No es necesario POO.

--- data_analyzer02.py
+++ data_analyzer02.py
@@ -28,6 +28,6 @@ print("Total landmarks: ",total_landmarks)
 for i in range(0,total_geofences):
     for j in range(0,total_landmarks):
         if geofences.iloc[i]==landmarks.iloc[j]:
-            print("\n")
+            print("\ncdz")
             print("Se encontró geofence con igual nombre de landmark")
             print("Geofence número: ",i,"con nombre: ",geofences.iloc[i],". ","Landmark número: ",j,"con nombre: ",landmarks.iloc[j],".")
