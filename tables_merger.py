83ad63652553984cc978cfbb4aa6db3db170d03d
jlhb1984 <jlhb1984@gmail.com>
2024-10-03 11:01:31 -0500

    Se fusionaron 2 tablas exitosamente.

--- tables_merger.py
+++ tables_merger.py
@@ -7,17 +7,27 @@ class Tables_merger:
         self.number_of_tables=number_of_tables
             
     def merger(number_of_tables):
-        table_name=input("Digita el nombre de la tabla: ")
-        df_concat=pd.read_csv('TSO Colombia - IBO IDCOM.csv')
-        print(df_concat.info())
-        print("Unit types: ")
-        print(df_concat['Unit Type'])
-        #for i in range(0,number_tables):
-        #    df_aux=pd.read_csv(table_name)
-        #    df_concat=df_concat.concat(df_aux)
-        #print(df_concat.info())
-
-    
+               
+        if number_of_tables==1:
+            table_name=input("\nDigita el nombre de la tabla 1:")
+            df=pd.read_csv(table_name)          
+            df_unit_types=df['Unit Type']
+            print(df_unit_types)
+        
+        elif number_of_tables>1:
+            table_name=input("\nDigita el nombre de la tabla 1:")
+            df=pd.read_csv(table_name)
+            df_unit_types=df['Unit Type']
+            print("\nUnits en table 1:\n",df_unit_types)
+                        
+            for i in range(1,number_of_tables):
+                table_name=input("\nDigita el nombre de la tabla:")
+                df_aux=pd.read_csv(table_name)
+                df_aux_unit_types=df_aux['Unit Type']
+                print("\nUnite types before concat:\n",df_unit_types.info())
+                df_concat=pd.concat([df_unit_types,df_aux_unit_types],axis=0)
+                print("\nUnite types after concat:\n")
+                print(df_concat.info())   
 
 
 
