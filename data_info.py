3e8ef0ffbb37c7f99b85cdb5d09d0ac661c396fa
jlhb1984 <jlhb1984@gmail.com>
2024-10-08 15:44:08 -0500

    Se logró eliminar registros con Phone Number y IMEI que no cumplen los parámetros.

--- data_info.py
+++ data_info.py
@@ -31,23 +31,29 @@ class Data_info:
             if len(str(df_table_info.iloc[i]['Phone Number']))<number_of_chars_pn:
                 print("\nSe encontró registros Phone Number con longitud inferior a la buscada.\n")
                 print(df_table_info.iloc[i])
-                df_phone_number=df_table_info.iloc[i]
+                #df_phone_number=df_table_info.iloc[i]
         number_of_chars_im=int(input("\nDigita el número de caracteres a detectar en el campo IMEI: \n"))
         for j in range(0,total_rows):
             if len(str(df_table_info.iloc[j]['IMEI']))<number_of_chars_im:
                 print("\nSe encontró registros IMEI con longitud inferior a la buscada.\n")
                 print(df_table_info.iloc[j])
-                df_imei=df_table_info.iloc[j]
+                #df_imei=df_table_info.iloc[j]
         option_drop=input("\nDeseas eliminar los registros que no cumplen la cantidad de caracteres requeridos: S/N \n")
-        if option_drop=='S':            
+        if option_drop=='S':
             print(total_rows)
             print(df_table_info.info()) 
-            df_table_info=df_table_info.drop(0)
-            """
+            df_table_info_aux=df_table_info
+           
             for i in range(0,total_rows):
                 if len(df_table_info.iloc[i]['Phone Number'])<number_of_chars_pn:
-                    df_table_info=df_table_info.drop(0,axis=0)    
-            """
-            print(df_table_info.info())                 
+                    df_table_info_aux=df_table_info_aux.drop(i)
+            #df_table_info=df_table_info_aux
+            total_rows=len(df_table_info_aux)
+            df_table_info_aux_f=df_table_info_aux
+            for i in range(0,total_rows):
+                if (len(str(df_table_info_aux.iloc[i]['IMEI'])))<number_of_chars_im:
+                    df_table_info_aux_f=df_table_info_aux_f.drop(i)
+
+        print(df_table_info_aux_f.info())                 
         
                     
